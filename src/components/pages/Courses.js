0e77ce130777cd95c97cb74e67351030f581001a
Midhun-chandran <midhun.gitlab@gmail.com>
2022-03-06 16:59:05 +0530

    Update Courses.js

--- src/components/pages/Courses.js
+++ src/components/pages/Courses.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../../App.css';
 import Course from '../Courses';
+import Footer from '../Footer';
 
 export default function Courses() {
-  return (<Course />) ;
+  return (<>
+  < Course/>
+     <Footer/>
+  </>) ;
 }
